e9088c5c543a56107f9a549448603e3768afc44f
johngiorgi <johnmgiorgi@gmail.com>
2019-04-24 09:36:34 -0400

    :recycle: Lint

--- src/cli/build_graph.py
+++ src/cli/build_graph.py
@@ -1,6 +1,6 @@
 import argparse
-import os
 import pickle
+
 import torch
 
 from ..preprocessor_graph import BuildGraph
@@ -9,7 +9,7 @@ from ..utils.generic_utils import make_dir
 
 def main(input_directory, output_directory):
     """Creates a graph set given the preprocessed Wiki- or MedHop data whose path is
-    given by `input_directory`. The resulting combined graph tensors and index 
+    given by `input_directory`. The resulting combined graph tensors and index
     dictionary are saved in `output_directory`.
     """
 
@@ -22,6 +22,7 @@ def main(input_directory, output_directory):
 
     torch.save(graphs, output_directory + 'graphs.t')
     pickle.dump(idxs, open(output_directory + 'sample_indices.pickle', 'wb'))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=('Builds the required graphs for each sample'
