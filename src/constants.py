19d70bac42c7bda271fc3dcc4a7d601dd61836ff
johngiorgi <johnmgiorgi@gmail.com>
2019-05-06 19:08:29 -0400

    :wrench: Make en-lg the default SpaCy model

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,12 @@
-# The spaCy model to load. Defaults to the medium sized English model.
-SPACY_MODEL = 'en_core_web_md'
-# Pre-trained BERT models. See here: https://github.com/huggingface/pytorch-pretrained-BERT 
+# The spaCy model to load. Defaults to the large sized English model.
+SPACY_MODEL = 'en_core_web_lg'
+
+# Greedyness of NeuralCoref. See here: https://github.com/huggingface/neuralcoref#parameters
+NEURALCOREF_GREEDYNESS = 0.40
+
+# Pre-trained BERT models. See here: https://github.com/huggingface/pytorch-pretrained-BERT
 # for more information.
-PRETRAINED_BERT_MODELS= [
+PRETRAINED_BERT_MODELS = [
     'bert-base-uncased',
     'bert-large-uncased',
     'bert-base-cased',
@@ -11,8 +15,12 @@ PRETRAINED_BERT_MODELS= [
     'bert-base-multilingual-cased',
     'bert-base-chinese',
 ]
+# The default pre-trained BERT model to use
+PRETRAINED_BERT_MODEL = 'bert-base-uncased'
 
-# Special token representing a sequence pad
-PAD = '[PAD]'
-CLS = '[CLS]'
-SEP = '[SEP]'
+PAD = '[PAD]'  # Special token representing a sequence pad
+CLS = '[CLS]'  # Special BERT classification token
+SEP = '[SEP]'  # Special BERT sequence seperator token
+
+# Maximum size of a training example, and examples exceeding this size are skipped.
+TRAIN_SIZE_THRESHOLD = 10000
